137dbaec917125c1081dd90cd9b353b3834cc928
Alex Tomlins <alex@tomlins.org.uk>
2017-01-22 22:25:46 +0000

    Add instruction type containing function pointer
    Avoids the need for evaluating the switch statement on every iteration,
    this can be done at parse time instead.

--- main.go
+++ main.go
@@ -1,76 +1,107 @@
 package main
 
 import (
+	"bufio"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 )
 
 const cellCount = 30000
 
+type instruction struct {
+	opCode byte
+	op     func()
+}
+
 type machine struct {
 	memory       [cellCount]byte
 	dp           uint
-	instructions []byte
+	instructions []instruction
 	ip           uint
 	input        io.Reader
 	output       io.Writer
 }
 
+func (m *machine) incData()    { m.memory[m.dp] += 1 }
+func (m *machine) decData()    { m.memory[m.dp] -= 1 }
+func (m *machine) incDataPtr() { m.dp += 1 }
+func (m *machine) decDataPtr() { m.dp -= 1 }
+func (m *machine) readInput() {
+	_, err := m.input.Read(m.memory[m.dp : m.dp+1])
+	if err != nil {
+		log.Fatalln("Error reading", err)
+	}
+}
+func (m *machine) writeOutput() {
+	_, err := m.output.Write(m.memory[m.dp : m.dp+1])
+	if err != nil {
+		log.Fatalln("Error writing", err)
+	}
+}
+func (m *machine) loop() {
+	if m.memory[m.dp] == 0 {
+		depth := 1
+		for depth != 0 {
+			m.ip++
+			switch m.instructions[m.ip].opCode {
+			case '[':
+				depth++
+			case ']':
+				depth--
+			}
+		}
+	}
+}
+
+func (m *machine) endLoop() {
+	if m.memory[m.dp] != 0 {
+		depth := 1
+		for depth != 0 {
+			m.ip--
+			switch m.instructions[m.ip].opCode {
+			case ']':
+				depth++
+			case '[':
+				depth--
+			}
+		}
+	}
+}
+
 func (m *machine) loadProgram(prog io.Reader) (err error) {
-	m.instructions, err = ioutil.ReadAll(prog)
-	return err
+	scanner := bufio.NewScanner(prog)
+	scanner.Split(bufio.ScanBytes)
+	for scanner.Scan() {
+		i := instruction{opCode: scanner.Text()[0]}
+		switch i.opCode {
+		case '>':
+			i.op = m.incDataPtr
+		case '<':
+			i.op = m.decDataPtr
+		case '+':
+			i.op = m.incData
+		case '-':
+			i.op = m.decData
+		case '.':
+			i.op = m.writeOutput
+		case ',':
+			i.op = m.readInput
+		case '[':
+			i.op = m.loop
+		case ']':
+			i.op = m.endLoop
+		default:
+			continue // All other characters ignored
+		}
+		m.instructions = append(m.instructions, i)
+	}
+	return scanner.Err()
 }
 
 func (m *machine) run() {
 	for int(m.ip) < len(m.instructions) {
-		switch m.instructions[m.ip] {
-		case '>':
-			m.dp++
-		case '<':
-			m.dp--
-		case '+':
-			m.memory[m.dp]++
-		case '-':
-			m.memory[m.dp]--
-		case '.':
-			_, err := m.output.Write(m.memory[m.dp : m.dp+1])
-			if err != nil {
-				log.Fatalln("Error writing", err)
-			}
-		case ',':
-			_, err := m.input.Read(m.memory[m.dp : m.dp+1])
-			if err != nil {
-				log.Fatalln("Error reading", err)
-			}
-		case '[':
-			if m.memory[m.dp] == 0 {
-				depth := 1
-				for depth != 0 {
-					m.ip++
-					switch m.instructions[m.ip] {
-					case '[':
-						depth++
-					case ']':
-						depth--
-					}
-				}
-			}
-		case ']':
-			if m.memory[m.dp] != 0 {
-				depth := 1
-				for depth != 0 {
-					m.ip--
-					switch m.instructions[m.ip] {
-					case ']':
-						depth++
-					case '[':
-						depth--
-					}
-				}
-			}
-		}
+		m.instructions[m.ip].op()
 		m.ip++
 	}
 }
